79553ff0ddfd933d1942c953643e55960432cdc3
Haotian Wang <hwang537@bloomberg.net>
2017-02-20 21:07:13 -0800

    changed enron.py

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,49 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+print len (enron_data)
+print len (enron_data['METTS MARK'])
+count = 0
+for k in enron_data :
+    if enron_data[k]['poi'] :
+        count += 1
+print count
 
+count = 0
+for k in enron_data :
+    if enron_data[k]['salary'] != 'NaN' :
+        count += 1
+print count
 
+count = 0
+for k in enron_data :
+    if enron_data[k]['email_address'] != 'NaN' :
+        count += 1
+print count
+
+count  = 0
+for k in enron_data :
+    if enron_data[k]['total_payments'] == 'NaN' and enron_data[k]['poi'] :
+        count += 1
+print (float(count) / 21)
+
+count  = 0
+for k in enron_data :
+    if enron_data[k]['total_payments'] == 'NaN' :
+        count += 1
+print (float(count) / 21)
+
+poi_names = open("../final_project/poi_names.txt").read().split('\n')
+poi_y = [name for name in poi_names if "(y)" in name or "(n)" in name]
+print len (poi_y)
+
+print (enron_data['PRENTICE JAMES'])
+print (enron_data['COLWELL WESLEY'])
+print (enron_data["SKILLING JEFFREY K"])
+SKILLING = enron_data["SKILLING JEFFREY K"]["total_payments"]
+LAY = enron_data["LAY KENNETH L"]["total_payments"]
+FASTOW = enron_data["FASTOW ANDREW S"]["total_payments"]
+
+print SKILLING
+print LAY
+print FASTOW
